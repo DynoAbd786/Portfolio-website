ec74e0cbfab406cabf20f4bd2c4d607de5ae50d0
DynoAbd786_Uni <sc22makk@leeds.ac.uk>
2025-08-10 19:04:47 +0100

    fixing viewport errors

--- website/obj/Debug/net8.0/website.AssemblyInfo.cs
+++ website/obj/Debug/net8.0/website.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fae36e8e80d64678e71e073bd0eb305b15e2f33")]
 [assembly: System.Reflection.AssemblyProductAttribute("website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
